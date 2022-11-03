ed9087917a3954239ec327d0b353207f35c2e27e
Roman Bybyk <64772371+rom4keee@users.noreply.github.com>
2022-11-03 21:46:50 +0200

    Update APItest.java

--- src/test/java/APItest.java
+++ src/test/java/APItest.java
@@ -1,26 +1,18 @@
 import API.Config;
-import API.Create;
 import API.Update;
 import API.Users;
 
-
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Locale;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-
-class ApiTests {
+class APItest {
     static String endpoint = "https://reqres.in";
 
     @BeforeAll
@@ -29,7 +21,7 @@ class ApiTests {
     }
 
     @Test
-   void VerifyAllUserData() {
+    void testAllUsers() {
         given()
                 .when().get("/api/users/")
                 .then().log().body()
@@ -41,17 +33,22 @@ class ApiTests {
                 .body("data.last_name", notNullValue())
                 .body("data.avatar", notNullValue());
     }
-
-    public String formattedTime() {
-        return ZonedDateTime
-                .now()
-                .format(DateTimeFormatter
-                        .ofPattern("EEE, dd MMM yyyy HH:mm:ss", Locale.ENGLISH)
-                        .withZone(ZoneOffset.UTC));
+    @Test
+    public void testSingleUserPage() {
+                 given()
+                .when()
+                .get("/api/users/2")
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .body("data.id", equalTo(2))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.last_name", equalTo("Weaver"))
+                .body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"));
     }
 
     @Test
-    void verifyPerPageParameter() {
+    public void testPerParameter() {
         int numberOfUsersPerPage = given()
                 .when().get("/api/users?page=1&per_page=2")
                 .then().extract().body().jsonPath().get("per_page");
@@ -66,21 +63,21 @@ class ApiTests {
     }
 
     @Test
-    void verifyUserIsUpdate() {
+    public void testUserIsUpdate() {
         given().body(new Update("morpheus", "zion resident"))
                 .when().put("/api/users/2")
                 .then().log().body().assertThat().statusCode(200).body("job", equalTo("zion resident"));
     }
 
     @Test
-    void verifyUserIsDelete() {
+    public void testUserIsDelete() {
         given()
                 .when().delete("/api/users/2")
                 .then().assertThat().statusCode(204);
     }
 
     @Test
-    void verifyAllUsersAreReturned() {
+    public void testAllUsersAreReturned() {
         int totalNumberOfUsers = given()
                 .when().get("/api/users?page=2")
                 .then().extract().body().jsonPath().get("total");
